6333f16a1d46c884256be36480ca3c6e1721e5ab
EimiRosalia14 <eimirosalia1567@gmail.com>
2024-11-20 19:11:13 -0400

    Agregando Entidades

---
+++ LoadDWHNorthwindOrders.Data/Entities/DWHNorthwindOrders/DimEmployees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadDWHNorthwindOrders.Data.Entities.DWHNorthwindOrders
+{
+    public class DimEmployees
+    {
+        public int EmployeeID { get; set; } 
+        public string LastName { get; set; } 
+        public string FirstName { get; set; } 
+        public string Title { get; set; } 
+        public string TitleOfCourtesy { get; set; } 
+        public DateTime? BirthDate { get; set; } 
+        public DateTime? HireDate { get; set; } 
+        public string TerritoryID { get; set; }
+    }
+}
---
+++ LoadDWHNorthwindOrders.Data/Entities/DWHNorthwindOrders/DimShippers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadDWHNorthwindOrders.Data.Entities.DWHNorthwindOrders
+{
+    public class DimShippers
+    {
+        public int ShipperID { get; set; }
+        public string CompanyName { get; set; } 
+        public string Phone { get; set; }
+    }
+}
